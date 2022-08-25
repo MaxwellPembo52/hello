a42cb98d97e9ed47f668153859f6cf4fa20e14e4
MaxwellPembo52 <mpembo2@huskers.unl.edu>
2022-08-25 19:46:18 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Maxwell Pembo
+ * Date: 2020/8/20
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Maxwell!\n");
+
+  return 0;
+}
